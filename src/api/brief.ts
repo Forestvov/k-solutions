e3e9a1a95632ea1a51687ce35d4d17c671e8ea53
forestvov1 <kukva2019@gmail.com>
2024-05-05 20:11:15 +1000

    fix

--- src/api/brief.ts
+++ src/api/brief.ts
@@ -316,7 +316,7 @@ export function useGetAnaliticHistoryGain({ page, pageSize }: IPagination) {
             },
             dataError: error,
             dataValidating: isValidating,
-            dataEmpty: !isLoading && !data.length,
+            dataEmpty: !isLoading && !data?.page?.content?.length,
             mutate,
         }),
         [error, isLoading, isValidating, mutate, data]
