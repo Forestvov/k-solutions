baae0ed98b43dc82eec09950a8670acc98d3db1f
forestvov1 <kukva2019@gmail.com>
2024-05-14 15:05:45 +1000

    fix

--- src/layouts/profile-layout.tsx
+++ src/layouts/profile-layout.tsx
@@ -1,6 +1,4 @@
-import { Outlet, useParams } from 'react-router';
-import { NavLink } from 'react-router-dom';
-import styled from '@emotion/styled';
+import { Outlet } from 'react-router';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 
@@ -8,27 +6,8 @@ import PersonalHeader from 'components/layouts/personal-header';
 import PersonalAside from 'components/layouts/personal-aside';
 import { useScrollToTop } from 'helpers/useScrollToTop';
 
-const Link = styled(NavLink)`
-    position: fixed;
-    right: 20px;
-    bottom: 100px;
-    font-size: 16px;
-    text-decoration: none;
-    color: #000;
-    background: #fff;
-    padding: 5px 10px;
-    border-radius: 12px;
-    box-shadow: 2px 1px 8px -3px rgba(0, 0, 0, 0.1);
-
-    @media (min-width: 1280px) {
-        bottom: 20px;
-    }
-`;
-
 const ProfileLayout = () => {
     useScrollToTop();
-
-    const { userId } = useParams();
 
     return (
         <Stack
@@ -49,7 +28,6 @@ const ProfileLayout = () => {
                         <Outlet />
                     </Stack>
                 </Stack>
-                {userId && <Link to="/admin/user">Обратно в админ панель</Link>}
             </Box>
         </Stack>
     );
