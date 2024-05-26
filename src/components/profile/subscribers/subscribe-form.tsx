b074830121c0e18a9032ef9dcbc0873eaa5a5401
forestvov1 <kukva2019@gmail.com>
2024-05-26 15:51:59 +1000

    add slide message

--- src/components/profile/subscribers/subscribe-form.tsx
+++ src/components/profile/subscribers/subscribe-form.tsx
@@ -1,16 +1,23 @@
+import type { ComponentType, ReactElement } from 'react';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useTranslation } from 'react-i18next';
 import InputMask from 'react-input-mask';
+
+import { yupResolver } from '@hookform/resolvers/yup';
 import styled from '@emotion/styled';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Fade from '@mui/material/Fade';
 import Typography from '@mui/material/Typography';
+import type { SlideProps } from '@mui/material/Slide';
+import Slide from '@mui/material/Slide';
+import Snackbar from '@mui/material/Snackbar';
+import type { TransitionProps } from '@mui/material/transitions';
 
 import validateSubscribe from 'helpers/validation/validateSubscribe';
 
 import { addContact } from 'api/order-contact';
-import { useTranslation } from 'react-i18next';
 
 interface Inputs {
     phoneNumber: string;
@@ -40,12 +47,32 @@ const Input = styled(InputMask)`
     }
 `;
 
+function SlideTransition(props: SlideProps) {
+    return <Slide {...props} direction="left" />;
+}
+
+type TransitionType = ComponentType<TransitionProps & { children: ReactElement<any, any> }>;
+
 const SubscribeForm = () => {
     const { t } = useTranslation('form');
 
-    const [success, setSuccess] = useState(false);
     const [error, setError] = useState<boolean | string>(false);
     const [load, setLoad] = useState(false);
+
+    const [state, setState] = useState<{
+        open: boolean;
+        Transition: TransitionType;
+    }>({
+        open: false,
+        Transition: Fade,
+    });
+
+    const handleClose = () => {
+        setState({
+            ...state,
+            open: false,
+        });
+    };
 
     const resolver = yupResolver(validateSubscribe);
 
@@ -59,13 +86,15 @@ const SubscribeForm = () => {
     });
 
     const onSubmit = async (data: Inputs) => {
-        setSuccess(false);
         setError(false);
         setLoad(true);
 
         try {
             await addContact(data);
-            setSuccess(true);
+            setState({
+                open: true,
+                Transition: SlideTransition,
+            });
         } catch (e) {
             // @ts-ignore
             if (e.response.data.message === 'This number is existed') {
@@ -77,47 +106,57 @@ const SubscribeForm = () => {
         await reset();
     };
     return (
-        <Stack spacing="20px">
-            <Stack
-                direction={{ xl: 'row', md: 'column', sm: 'row' }}
-                spacing={{ xl: '30px', md: '20px', xs: '20px' }}
-                alignItems={{ xl: 'center' }}
-                component="form"
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <Controller
-                    name="phoneNumber"
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field: { ref, onChange, value } }) => (
-                        <Input
-                            ref={ref}
-                            mask="+9 (999) 999-99-99"
-                            value={value}
-                            onChange={onChange}
-                            placeholder={t('Введите номер телефона')}
-                        />
-                    )}
-                />
-                <Button
-                    variant="green"
-                    type="submit"
-                    disabled={load}
-                    sx={{
-                        width: { lg: '278px' },
-                        flex: '0 0 auto',
-                        padding: {
-                            lg: '20px 45px',
-                            md: '16px 20px',
-                        },
-                    }}
+        <>
+            <Stack spacing="20px">
+                <Stack
+                    direction={{ xl: 'row', md: 'column', sm: 'row' }}
+                    spacing={{ xl: '30px', md: '20px', xs: '20px' }}
+                    alignItems={{ xl: 'center' }}
+                    component="form"
+                    onSubmit={handleSubmit(onSubmit)}
                 >
-                    {t('Заказать Консультацию')}
-                </Button>
+                    <Controller
+                        name="phoneNumber"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field: { ref, onChange, value } }) => (
+                            <Input
+                                ref={ref}
+                                mask="+9 (999) 999-99-99"
+                                value={value}
+                                onChange={onChange}
+                                placeholder={t('Введите номер телефона')}
+                            />
+                        )}
+                    />
+                    <Button
+                        variant="green"
+                        type="submit"
+                        disabled={load}
+                        sx={{
+                            width: { lg: '278px' },
+                            flex: '0 0 auto',
+                            padding: {
+                                lg: '20px 45px',
+                                md: '16px 20px',
+                            },
+                        }}
+                    >
+                        {t('Заказать Консультацию')}
+                    </Button>
+                </Stack>
+                {error && <Typography variant="body1">{error}</Typography>}
             </Stack>
-            {success && <Typography variant="body1">{t('Заявка успешно отправлена')}</Typography>}
-            {error && <Typography variant="body1">{error}</Typography>}
-        </Stack>
+            <Snackbar
+                open={state.open}
+                onClose={handleClose}
+                TransitionComponent={state.Transition}
+                message={t('Ожидайте с вами свяжется специалист')}
+                key={state.Transition.name}
+                autoHideDuration={3000}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            />
+        </>
     );
 };
 
