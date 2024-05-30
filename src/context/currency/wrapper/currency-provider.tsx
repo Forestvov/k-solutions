de07a1ab607d0dcd5e8e8637d04df2a4aa45d9d1
forestvov1 <kukva2019@gmail.com>
2024-05-31 00:22:14 +1000

    fix curs list

--- src/context/currency/wrapper/currency-provider.tsx
+++ src/context/currency/wrapper/currency-provider.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { getCookie, setCookie } from 'context/settings/cookie';
 
 import { CurrencyContext } from './currency-context';
+import { HOST_API } from '../../../config-global';
 
 // ----------------------------------------------------------------------
 
@@ -24,7 +25,7 @@ export function CurrencyProvider({ children }: Props) {
 
     const initialize = useCallback(async () => {
         try {
-            const res = await axios('/curs/list');
+            const res = await axios(`${HOST_API}/curs/list`);
             const resJson = res.data.Valute;
 
             setCurrency({
