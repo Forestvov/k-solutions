96f900a1c286e44b1ebd3d893f50345ca0f4d70c
forestvov1 <kukva2019@gmail.com>
2024-06-25 23:30:08 +1000

    fix hidden history

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -37,7 +37,7 @@ export function useGetHistoryTransaction({ page, pageSize, transactionType, tran
                 size: pageSize,
                 sortDir: 'DESC',
                 sortField: 'transactionDate',
-                criteria: [...criteria, ...[{ key: 'transactionStatus', operation: '!=', value: 'hidden' }]],
+                criteria: [...criteria, ...[{ key: 'transactionStatus', operation: '!=', value: 'Hidden' }]],
             },
             'POST',
             {
